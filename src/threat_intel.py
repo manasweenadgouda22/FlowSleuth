34c41303996c4da9f566509d92ec581bf7c3bdd6
Manaswee Nadgouda <manaswee.nadgouda22@gmail.com>
2025-11-19 09:09:08 -0700

    Update threat_intel.py

--- src/threat_intel.py
+++ src/threat_intel.py
@@ -1,11 +1,5 @@
-import pandas as pd
-
 def enrich_with_threat_intel(df):
-    if df.empty:
-        return df
-
-    df = df.copy()
     df["threat_label"] = df["dst_ip"].apply(
-        lambda ip: "KNOWN BOTNET" if ip.startswith("45.") else "UNKNOWN"
+        lambda ip: "Known Malware Server" if str(ip).startswith("91.") else "Clean"
     )
     return df
