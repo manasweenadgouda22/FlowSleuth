533803fac8bd6b7f13354b892e26c8153b7e270e
Manaswee Nadgouda <manaswee.nadgouda22@gmail.com>
2025-11-19 09:08:41 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,16 +1,23 @@
 FLOW_REQUIRED_COLUMNS = [
-    "timestamp", "src_ip", "dst_ip", "dst_port", "bytes", "file_type"
+    "timestamp",
+    "src_ip",
+    "dst_ip",
+    "dst_port",
+    "bytes",
+    "file_type"
 ]
 
 FIREWALL_REQUIRED_COLUMNS = [
-    "timestamp", "src_ip", "dst_ip", "action", "rule"
+    "timestamp",
+    "src_ip",
+    "dst_ip",
+    "dst_port",
+    "action",
+    "rule_name"
 ]
 
-HIGH_RISK_EXTENSIONS = [
-    ".exe", ".dll", ".bat", ".ps1", ".js", ".vbs", ".sh", ".zip", ".rar"
-]
+HIGH_RISK_EXTENSIONS = [".exe", ".dll", ".bat", ".ps1", ".sh"]
+SUSPICIOUS_PORTS = [21, 22, 23, 445, 3389]
 
-SUSPICIOUS_PORTS = [22, 23, 445, 3389, 5900, 8080]
-
-MIN_BYTES_FOR_DOWNLOAD = 150000  # ~150 KB
+MIN_BYTES_FOR_DOWNLOAD = 50000
 MIN_CONNECTIONS_FOR_BEACON = 5
